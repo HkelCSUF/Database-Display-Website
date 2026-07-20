0ecf4af7081bf894f3afe6e1f7890f4e6bb22906
Hayden Kelley <hayden.e.kelley@gmail.com>
2026-07-20 10:38:21 -0700

    refactor
    do the useEffect and useState correctly

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { getMockarooData, getUselessFact } from "./api.js"
 
-const fact = getUselessFact();
+
 // Deal with mock stuff later. also do a better format for the fact API. 
+// Mockaroo has a limit of 200 requests.
+// Firebase it is then.
 
 export default function App() {
 
-    
+    const [fact, setFact] = useState("");
+    const [mock, setMock] = useState([]);
+    useEffect(() => {
+        setFact(getUselessFact());
+        // setMock(getMockarooData());
+    }, [])
     console.log(fact);
+    console.log(mock);
 
     return (
         <>
